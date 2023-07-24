ba65620831be7a3ff360dbedc5e561c8db97c91b
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2023-07-24 20:28:32 +0200

    Writing module to file - part 1

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define READ_SPAN 16384
+#define NEWLINE "\r\n"
 
 typedef enum {false, true} bool;
 
 char* str_skip_line(char* buffer, char* cursor)
 {
-    cursor = strstr(buffer, "\r\n");
+    cursor = strstr(buffer, NEWLINE);
     if (cursor == NULL || *(cursor+2) == '\0')
         return NULL;
     return cursor + 2;
@@ -40,7 +40,7 @@ void str_tag_extract_attr(char* tag, const char* attr_name, char* out)
     attr_cursor_end = strchr(attr_cursor_begin, '=');
     if (!attr_cursor_end)
         return;
-    
+
     bool attr_names_match = true;
     for (int i = 0; *(attr_name + i) != '\0' && attr_cursor_begin + i != attr_cursor_end; i++)
         attr_names_match &= *(attr_name + i) == *(attr_cursor_begin + i);
@@ -51,62 +51,81 @@ void str_tag_extract_attr(char* tag, const char* attr_name, char* out)
     attr_cursor_begin = strchr(attr_cursor_end, '\"');
     if (!attr_cursor_begin)
         return;
-    
+
     attr_cursor_begin++;
     attr_cursor_end = strchr(attr_cursor_begin, '\"');
     if (!attr_cursor_end)
         return;
-    
+
     str_copy_from_to_nullt(attr_cursor_begin, attr_cursor_end, out);
 }
 
+// TODO: Add a backups
 int main()
 {
-    FILE* f = fopen("project.gmx", "rb");
-    if (!f)
+    char* project;
+
+    {
+        FILE* f = fopen("project.gmx", "rb");
+        if (!f)
+        {
+            return 1;
+        }
+
+        fseek(f, 0, SEEK_END);
+        unsigned length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        if (!length)
+        {
+            return 1;
+        }
+
+        project = malloc(length+1);
+        if (!project)
+        {
+            return 1;
+        }
+        *(project+length) = '\0';
+
+        fread(project, sizeof(char), length, f);
+        if (ferror(f))
+        {
+            return 1;
+        }
+
+        fclose(f);
+    }
+
+    FILE* module_out = fopen("module.txt", "wb");
+    if (ferror(module_out))
     {
         return 1;
     }
 
-    fseek(f, 0, SEEK_END);
-    unsigned length = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    if (!length)
-    {
-        return 1;
-    }
-
-    char* buffer = malloc(length+1);
-    if (!buffer)
-    {
-        return 1;
-    }
-    *(buffer+length) = '\0';
-
-    fread(buffer, sizeof(char), length, f);
-    if (ferror(f))
-    {
-        return 1;
-    }
-
-    fclose(f);
+    char* resource_type = "objects";
 
     // Test string for later
-    //buffer = 
+    //project =
     //    "<!-- lol comment-->\r\n"
     //    "<assets>\r\n"
     //        "<object name=\"named\">hellllooooo</object>\r\n"
     //    "</assets>\0";
-    char* cursor = buffer;
+    char* cursor = project;
+    char* tag_begin_cursor;
     char* tag_content_cursor;
     char* tag_name_begin_cursor;
     char* tag_name_end_cursor;
+
+    int group_depth;
+    char* group_cursor;
+    bool capturing_group = false;
+
     char tag_content[1024];
     char tag_name[128];
     char attr_name[128];
     unsigned depth = 0;
-
-    cursor = str_skip_line(buffer, cursor);
+    
+    cursor = str_skip_line(project, cursor);
 
     while (cursor)
     {
@@ -117,51 +136,92 @@ int main()
         cursor = strchr(cursor, '<');
         if (!cursor)
             break;
-        
+
+        tag_begin_cursor = cursor;
 
         if (*(++cursor) == '\0')
             break;
 
         tag_content_cursor = cursor;
 
+        bool depth_increase;
         if (*cursor == '/')
         {
             depth--;
             cursor++;
+            depth_increase = false;
         }
         else
         {
             depth++;
+            depth_increase = true;
         }
 
         cursor = strchr(cursor, '>');
         if (!cursor)
             break;
-        
-        str_copy_from_to_nullt(tag_content_cursor, cursor, tag_content);
 
-        char* tag_content_space = strchr(tag_content, ' ');
-        if (tag_content_space)
-            str_copy_from_to_nullt(tag_content, tag_content_space, tag_name);
-        else
-            str_copy_from_to_nullt(tag_content_cursor, cursor, tag_name);
-
-        str_tag_extract_attr(tag_content, "name", attr_name);
-        
-        if (*attr_name != '\0')
+        if (depth_increase)
         {
-            printf(attr_name);
-            printf("\n");
+            // Look for tag attributes
+            str_copy_from_to_nullt(tag_content_cursor, cursor, tag_content);
+
+            char* tag_content_space = strchr(tag_content, ' ');
+            if (tag_content_space)
+                str_copy_from_to_nullt(tag_content, tag_content_space, tag_name);
+            else
+                str_copy_from_to_nullt(tag_content_cursor, cursor, tag_name);
+
+            str_tag_extract_attr(tag_content, "name", attr_name);
+
+            if (!capturing_group && strcmp(tag_name, resource_type) == 0)
+            {
+                group_cursor = tag_begin_cursor;
+                group_depth = depth;
+                capturing_group = true;
+            }
         }
         else
         {
-            printf("NO NAME ATTRIBUTE IN TAG: ");
-            printf(tag_content);
-            printf("\n");
+            if (capturing_group && depth < group_depth)
+            {
+                char* group_cursor_end = cursor + 1;
+                int group_size = 0;
+                for (char* ptr = group_cursor; ptr != group_cursor_end; ptr++)
+                    group_size++;
+
+                const char module_section_start_marker[] = "### MODULE SECTION START ###" NEWLINE;
+                const char module_section_header[] = "objects" "" NEWLINE;
+
+                fwrite(module_section_start_marker, 1, strlen(module_section_start_marker), module_out);
+                fwrite(module_section_header, 1, strlen(module_section_header), module_out);
+                fwrite(group_cursor, 1, group_size + 1, module_out);
+
+                while (*group_cursor_end != '\0')
+                {
+                    *(group_cursor_end - group_size) = *group_cursor_end;
+                    group_cursor_end++;
+                }
+
+                cursor = group_cursor;
+                capturing_group = false;
+            }
         }
     }
 
-    free(buffer);
+    fclose(module_out);
+
+    {
+        unsigned size = 0;
+        for (char* ptr = project; *ptr != '\0'; ptr++)
+            size++;
+        
+        FILE* f = fopen("project_modified.gmx", "wb");
+        fwrite(project, 1, size, f);
+        fclose(f);
+    }
+
+    free(project);
 
     return 0;
 }
